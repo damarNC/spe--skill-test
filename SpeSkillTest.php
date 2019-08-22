677bc9a5960909fa536196f37b1324db50e3c10c
Damar Nurcahya <nurcahya.damar@gmail.com>
2019-08-22 13:59:59 +0700

    blueOcean, findNeedle

--- SpeSkillTest.php
+++ SpeSkillTest.php
@@ -62,6 +62,27 @@ class SpeSkillTest {
 
     }
 
+    public function findNeedle($array,$string) {
+
+        $result = array_search($string, $array);
+
+        return $result;
+    }
+
+    public function blueOcean($array, $number) {
+
+
+        $reverse = array_reverse($number);
+
+        if($reverse[0]==1){
+            unset($array[0]);
+            $result = $array;
+        } else {
+            
+        }
+        return $result;
+    }
+
 
 }
 
@@ -72,9 +93,18 @@ echo "<br/>";
 echo $a->narcissistic(153);
 echo "<br/>";
 echo $a->narcissistic(1234);
+
+
 echo "<br/>";
 print_r($a->parityOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
 echo "<br/>";
 print_r($a->parityOutlier([160, 3, 1719, 19, 11, 13, -21]));
 echo "<br/>";
 print_r($a->parityOutlier([11, 13, 15, 19, 9, 13, -21]));
+
+
+echo "<br/>";
+print_r($a->findNeedle(["red", "blue", "yellow", "black", "grey"], "blue"));
+
+echo "<br/>";
+print_r($a->blueOcean([1,2,3,4,6,10], [1]));
